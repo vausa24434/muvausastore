237b40c29bc5c66af4f85f76736c63eea0f94bd8
vausa24434 <muvausastore1@gmail.com>
2024-08-04 06:13:57 +0700

    coba 6.13

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://api.digiflazz.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,12 +21,22 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000, // Mengatur port yang digunakan oleh server pengembangan
     proxy: {
       '/api': {
         target: 'https://api.digiflazz.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: 'dist', // Direktori output untuk build
+    sourcemap: true, // Menghasilkan sourcemaps untuk debugging
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Alias untuk folder src
     },
   },
 });
